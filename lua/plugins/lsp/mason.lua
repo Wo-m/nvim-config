875d8fdf72b435973b4dfb88c7b681ea372d8e37
dominic.thompson <dominic.thompson@vivcourt.com>
2024-12-20 08:23:59 +1000

    remove unused language servers

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -26,9 +26,6 @@ return {
         mason_lspconfig.setup({
           -- list of servers for mason to install
           ensure_installed = {
-            "tsserver",
-            "html",
-            "cssls",
             "lua_ls",
             "pyright",
             "clangd"
